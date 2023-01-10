2757b70cc50ac79e90357a934e38d5e5112e3143
Alonso Vargas <alonsodjesus.vargas.rincon@hotmail.com>
2023-01-09 20:57:25 -0600

    Formulario para los tweets

---
+++ adisa/src/Form.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { auth, db, saveTweet, onSnapshot} from "./firebase";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
+function Form() {
+    const datoInicial = {
+        mensaje: ""
+    }
+
+    const [twitters, setTwitters] = useState(datoInicial);
+
+    const nuevoMensaje = (a) => {
+        const {name, value} = a.target;
+        setTwitters({[name]: value}) 
+    }
+
+    const comprueba = () => {
+        console.log(twitters)
+    }
+
+    const publicarMensaje = (a) => {
+        a.preventDefault();
+        saveTweet(twitters);
+        setTwitters({...datoInicial});
+        a.target.reset();
+        
+    }
+
+  return (
+    <div>
+        <form className="new-publication" onSubmit={publicarMensaje}>
+            <h2>Home</h2>
+            <div className="publication-data">
+                <div className="profile-image"></div>
+                <textarea type="text" name='tweet' placeholder="What's happening" onChange={nuevoMensaje}/>
+            </div>
+
+            <hr />
+
+            <div className="buttons-publication">
+                <div className="publication-option"> <FontAwesomeIcon icon={icon({name: 'image', style: 'solid'})} className="publication-icon"/> </div>
+                <div className="publication-option"> <FontAwesomeIcon icon={icon({name: 'camera', style: 'solid'})} className="publication-icon"/> </div>
+                <div className="publication-option"> <FontAwesomeIcon icon={icon({name: 'list-ul', style: 'solid'})} className="publication-icon"/> </div>
+                <div className="publication-option"> <FontAwesomeIcon icon={icon({name: 'face-laugh-wink', style: 'solid'})} className="publication-icon"/> </div>
+                <div className="publication-option"> <FontAwesomeIcon icon={icon({name: 'calendar-days', style: 'solid'})} className="publication-icon"/> </div>
+                <div className="publication-option"> <FontAwesomeIcon icon={icon({name: 'location-dot', style: 'solid'})} className="publication-icon"/> </div>
+           
+                <button className="tweet">Tweet</button>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Form
